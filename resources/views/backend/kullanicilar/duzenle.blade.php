720655ef2d4884e1de9185d1ec7e814711d665b7
61burakbilici <burakbilici@hotmail.com.tr>
2019-12-11 17:10:29 +0300

    Hastane PHPStorn Deneme

--- resources/views/backend/kullanicilar/duzenle.blade.php
+++ resources/views/backend/kullanicilar/duzenle.blade.php
@@ -1,68 +1,136 @@
-@extends('backend.layout')
-
-@section('title')
-Kullanıcı Düzenleme Sayfası- MediRehber Admin Paneli | Burak BİLİCİ
-@endsection
-
-@section('content')
-<div class="content-wrapper">
+@extends('backend.layout') @section('title') Kullanıcı Düzenleme Sayfası- MediRehber Admin Paneli | Burak BİLİCİ @endsection @section('content')
+    <div class="content-wrapper">
 
 
-    <!-- Main content -->
-    <section class="content container-fluid">
+        <!-- Main content -->
+        <section class="content container-fluid">
 
-      
-        <div class="col-md-12">
-            <div class="box">
-              <div class="box-header with-border">
-                <h3 class="box-title">Düzeneleme</h3>
-  
-                <div class="box-tools pull-right">
-                  <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
-                  </button>
-                  
-                </div>
-              </div>
-              <!-- /.box-header -->
-              <div class="box-body">
-                <div class="row">
-                    <div class="col-md-12">
 
-                        <div class="box-body">
-                            <form action="" method="post" enctype="multipart/form-data">
-                              @csrf
+            <div class="col-md-12">
+                <div class="box">
+                    <div class="box-header with-border">
+                        <h3 class="box-title">Düzeneleme</h3>
 
-                              <div class="form-group">
-                                  <label>Açıklama</label>
-                                  <div class="row">
-                                      <div class="col-xs-12">
-                                          <input class="form-control" type="text"
-                                                 value="{{$kullanici->users_name }}">
-                                      </div>
-                                  </div>
-                              </div>
-          
-          
-                          </form>
-                          </div>
+                        <div class="box-tools pull-right">
+                            <button type="button" class="btn btn-box-tool" data-widget="collapse"><i
+                                    class="fa fa-minus"></i>
+                            </button>
+
+                        </div>
                     </div>
-                  
+                    <!-- /.box-header -->
+                    <div class="box-body">
+                        <div class="row">
+                            <div class="col-md-12">
+
+                                <div class="box-body">
+                                    <form action="" method="post" enctype="multipart/form-data">
+                                        @csrf
+                                        <div class="col-md-12">
+
+                                            <div class="col-md-6">
+                                                <div class="form-group">
+                                                    <label>Adı Soyadı</label>
+                                                    <div class="row">
+                                                        <div class="col-xs-12">
+                                                            <input class="form-control" type="text"
+                                                                   value="{{$kullanici->users_name }}">
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <div class="col-md-6">
+                                                <div class="form-group">
+                                                    <label>Kullanıcı</label>
+                                                    <div class="row">
+                                                        <div class="col-xs-12">
+                                                            <input class="form-control" type="text"
+                                                                   value="{{$kullanici->users_name }}">
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                        </div>
+                                        <div class="col-md-12">
+
+                                            <div class="col-md-6">
+                                                <div class="form-group">
+                                                    <label>Telefon Numarası</label>
+                                                    <div class="row">
+                                                        <div class="col-xs-12">
+                                                            <input class="form-control" type="text"
+                                                                   value="{{$kullanici->users_tel }}">
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <div class="col-md-6">
+                                                <div class="form-group">
+                                                    <label>Hastane</label>
+                                                    <div class="row">
+                                                        <div class="col-xs-12">
+                                                            <input class="form-control" type="text"
+                                                                   value="{{$kullanici->hastane_id }}">
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                        </div>
+                                        <div class="col-md-12">
+
+                                            <div class="col-md-6">
+                                                <div class="form-group">
+                                                    <label>Mail Adresi</label>
+                                                    <div class="row">
+                                                        <div class="col-xs-12">
+                                                            <input class="form-control" type="email"
+                                                                   value="{{$kullanici->users_email }}">
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <div class="col-md-6">
+                                                <div class="form-group">
+                                                    <label>Şifre</label>
+                                                    <div class="row">
+                                                        <div class="col-xs-12">
+                                                            <input class="form-control" type="password"
+                                                                   value="{{$kullanici->users_password }}">
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="box-footer">
+                                                <button style="width: 100%;" type="submit" class="btn btn-primary">
+                                                    Kaydet
+                                                </button>
+                                            </div>
+                                        </div>
+
+                                    </form>
+                                </div>
+                            </div>
+
+                        </div>
+                        <!-- /.row -->
+                    </div>
+
                 </div>
-                <!-- /.row -->
-              </div>
-             
+                <!-- /.box -->
             </div>
-            <!-- /.box -->
-          </div>
-          <!-- /.col -->
+            <!-- /.col -->
 
-    </section>
-    <!-- /.content -->
-  </div>    
-
+        </section>
+        <!-- /.content -->
+    </div>
 @endsection
 
-@section('css')    
+@section('css')
 @endsection
 
 @section('js')
--- resources/views/backend/kullanicilar/index.blade.php
+++ resources/views/backend/kullanicilar/index.blade.php
@@ -103,30 +103,4 @@ Kullanıcılar - MediRehber Admin Paneli | Burak BİLİCİ
 <!-- jQuery 3 -->
 <script src="/backend/bower_components/jquery/dist/jquery.min.js"></script>
 <!-- Bootstrap 3.3.7 -->
-<script src="/backend/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
-<!-- DataTables -->
-<script src="/backend/bower_components/datatables.net/js/jquery.dataTables.min.js"></script>
-<script src="/backend/bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
-<!-- SlimScroll -->
-<script src="/backend/bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
-<!-- FastClick -->
-<script src="/backend/bower_components/fastclick/lib/fastclick.js"></script>
-<!-- AdminLTE App -->
-<script src="/backend/dist/js/adminlte.min.js"></script>
-<!-- AdminLTE for demo purposes -->
-<script src="/backend/dist/js/demo.js"></script>
-<!-- page script -->
-<script>
-  $(function () {
-    $('#example1').DataTable()
-    $('#example2').DataTable({
-      'paging'      : true,
-      'lengthChange': false,
-      'searching'   : false,
-      'ordering'    : true,
-      'info'        : true,
-      'autoWidth'   : false
-    })
-  })
-</script>
 @endsection
